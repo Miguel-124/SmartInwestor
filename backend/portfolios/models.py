a6887d91f121dfd868515e2caae21f80083dc3e5
michalgorecki <michalgorecki124@gmail.com>
2025-08-31 21:48:42 +0200

    fix: add a blank line for improved readability in Portfolio model

--- backend/portfolios/models.py
+++ backend/portfolios/models.py
@@ -21,6 +21,7 @@ class Portfolio(models.Model):
                 name="uq_portfolio_owner_lower_name"
             )
         ]
+        
         indexes = [
             Index(fields=["owner", "name"]),
             Index(Lower("name"), name="idx_portfolio_lower_name"),
